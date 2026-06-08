22963b21ed11e9b85ab281817b3f3244ba9c4ae9
Keyboard1000n17 <keyboard1000n17@gmail.com>
2026-06-08 23:57:20 +0300

    Run `bun init`, run `bun add figlet`, start work on glyphs for headings
    regret has hit me like a wave, no a tsunami

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
+import figlet from "figlet";
+
 console.log(Bun.argv);
+
+async function stylize(text) {
+  const styled = await figlet.text(text, {
+    font: "ANSI Shadow",
+  });
+  return styled;
+}
 
 async function readFile(filepath) {
   const readFile = Bun.file(filepath);
   const text = await readFile.text();
-  console.log(text);
+  return text;
 }
 
-readFile(Bun.argv[2]);
+async function parseFile() {
+  const text = await readFile(Bun.argv[2]);
+  const arr = text.split("\n\n");
+  for (let a of arr) {
+    console.log(a);
+  }
+  const headings = arr.filter((heading) => /^#/.test(heading));
+  headings.forEach(async (h) => {
+    console.log(await stylize(h));
+  });
+}
+
+parseFile();
+
+// fonts to use
+// - terrace
+// - mono 9
+// - dos rebel
+// - coder mini
+// - rebel

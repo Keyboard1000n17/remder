68233f26b0b7961d75e94f309a8bafd5b98faf73
Keyboard1000n17 <keyboard1000n17@gmail.com>
2026-06-11 15:58:42 +0300

    Add proper rendering of glyphs, fix the output system

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 import figlet from "figlet";
 
-console.log(Bun.argv);
+async function getGlyphs() {
+  return await Bun.file("./chars.json").json();
+}
 
-async function stylize(text) {
-  const styled = await figlet.text(text, {
-    font: "ANSI Shadow",
-  });
+const glyphs = await getGlyphs();
+
+async function stylize(text, type) {
+  let styled;
+  if (type === "heading") {
+    const convertText = text.toUpperCase().split("");
+    let grid = [[], [], [], [], [], [], []];
+    for (let i = 0; i < convertText.length; i++) {
+      const character = convertText[i];
+      for (let j in grid) {
+        grid[j].push(glyphs.h1[character][j]);
+      }
+    }
+    const almostStyled = [];
+    for (let row of grid) {
+      almostStyled.push(row.join(""));
+    }
+    styled = almostStyled.join("\n");
+  } else if (type === "paragraph") {
+    styled = text;
+  }
   return styled;
 }
 
@@ -15,23 +34,29 @@ async function readFile(filepath) {
   return text;
 }
 
+const textComponents = [];
+
 async function parseFile() {
   const text = await readFile(Bun.argv[2]);
   const arr = text.split("\n\n");
+  let type;
   for (let a of arr) {
-    console.log(a);
+    if (/^#/.test(a)) type = "heading";
+    else if (/\s*-/.test(a)) type = "list-item";
+    else if (/\s*\d/.test(a)) type = "numbered-list-item";
+    else type = "paragraph";
+    textComponents.push({ type: type, content: a });
   }
-  const headings = arr.filter((heading) => /^#/.test(heading));
-  headings.forEach(async (h) => {
-    console.log(await stylize(h));
-  });
 }
 
-parseFile();
+async function printText() {
+  const stylizedText = [];
+  for (let el of textComponents) {
+    const stylized = await stylize(el.content, el.type);
+    stylizedText.push(stylized);
+  }
+  console.log(stylizedText.join("\n\n"));
+}
 
-// fonts to use
-// - terrace
-// - mono 9
-// - dos rebel
-// - coder mini
-// - rebel
+await parseFile();
+await printText();

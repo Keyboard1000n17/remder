0dc393d3f0fa099d062bac41354952a1a830052f
Keyboard1000n17 <keyboard1000n17@gmail.com>
2026-06-29 18:32:51 +0300

    Remove unnecessary object processing and dependencies

--- src/parse-input.js
+++ src/parse-input.js
@@ -1,38 +1,16 @@
 import MarkdownIt from "markdown-it";
-import Footnotes from "markdown-it-footnote";
 import GithubAlerts from "markdown-it-github-alerts";
-import Tables from "markdown-it-table";
 import TaskLists from "markdown-it-task-lists";
 
-function giveFields(obj) {
-  const fields = obj.map((token) => {
-    const toReturn = {
-      type: token.type,
-      tag: token.tag,
-      nesting: token.nesting,
-      content: token.content,
-      children: token.children,
-      info: token.info,
-    };
-    if (token.children) toReturn.children = giveFields(token.children);
-    return toReturn;
-  });
-
-  return fields;
-}
-
-export default async function parse(input) {
+export default function parse(input) {
   const md = new MarkdownIt({
     langPrefix: "langauge-",
     html: true,
     linkify: true,
   });
-  md.use(Footnotes);
   md.use(GithubAlerts);
-  md.use(Tables);
   md.use(TaskLists);
-  const modifiedInput = input.replaceAll(/<br( \/)?>/, "\n\n");
-  const tokenized = md.parse(modifiedInput);
-  const tokens = giveFields(tokenized);
+  const modifiedInput = input.replaceAll(/<br( \/)?>/g, "\n\n");
+  const tokens = md.parse(modifiedInput);
   return tokens;
 }

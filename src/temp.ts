33e1c916ff7bdf5a94af3e6707a3d99c19e34cdc
Keyboard1000n17 <keyboard1000n17@gmail.com>
2026-08-18 22:36:57 +0300

    Start using neo-neo-blessed and do a generational fuck up

--- src/temp.ts
+++ src/temp.ts
@@ -1,16 +1,16 @@
-import { Select, createCliRenderer } from "@opentui/core";
-
-const renderer = await createCliRenderer();
-
-const menu = Select({
-  width: 30,
-  height: 8,
-  options: [
-    { name: "Option 1", description: "First option" },
-    { name: "Option 2", description: "Second option" },
-    { name: "Option 3", description: "Third option" },
-  ],
-});
-
-menu.focus();
-renderer.root.add(menu);
+// import { Select, createCliRenderer } from "@opentui/core";
+//
+// const renderer = await createCliRenderer();
+//
+// const menu = Select({
+//   width: 30,
+//   height: 8,
+//   options: [
+//     { name: "Option 1", description: "First option" },
+//     { name: "Option 2", description: "Second option" },
+//     { name: "Option 3", description: "Third option" },
+//   ],
+// });
+//
+// menu.focus();
+// renderer.root.add(menu);

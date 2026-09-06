927cae9c0db571df0b00f9316b7289297b39f464
Keyboard1000n17 <keyboard1000n17@gmail.com>
2026-09-06 11:50:25 +0300

    Add tab cycling

--- src/renderer.ts
+++ src/renderer.ts
@@ -23,6 +23,8 @@ import {
   type TextOptions,
   ScrollBoxRenderable,
   SelectRenderable,
+  SelectRenderableEvents,
+  type SelectOption,
 } from "@opentui/core";
 import { parseArgs } from "node:util";
 import got from "got";
@@ -869,20 +871,21 @@ if (args.positionals.length > 0) {
     process.exit(0);
   } else {
     const renderables = await renderMarkdown(tokens as ProcessedToken[]);
-    renderables.forEach((renderable) => (renderable.marginBottom = 1));
-    const box = ScrollBox(
-      {
-        width: "auto",
-        minWidth: 0,
-        height: renderer.height - 1,
-        id: "root-scrollbox",
-        flexShrink: 1,
-        flexGrow: 1,
-        alignSelf: "flex-start",
-        paddingRight: 3,
-      },
-      renderables,
-    );
+    const box = new ScrollBoxRenderable(renderer, {
+      width: "auto",
+      minWidth: 0,
+      height: renderer.height - 1,
+      id: "root-scrollbox",
+      flexShrink: 1,
+      flexGrow: 1,
+      alignSelf: "flex-start",
+      paddingRight: 3,
+      // scrollbarOptions: { scrollStep: 1 },
+    });
+    renderables.forEach((renderable) => {
+      renderable.marginBottom = 1;
+      box.add(renderable);
+    });
     box.focus();
     root.add(box);
   }
@@ -998,6 +1001,7 @@ const tocMenuChildren = headingsArrayForToc.map((heading) => {
   return {
     name: `${"  ".repeat(heading.level - 1)}${headingNumber} ${heading.text}`,
     description: "",
+    value: heading.id,
   };
 });
 const tocMenu = new SelectRenderable(renderer, {
@@ -1070,8 +1074,7 @@ const keyHandler = (key: KeyEvent) => {
     if (!id) return;
     const heading = root.findDescendantById(id);
     const y = heading?.y;
-    if (y === undefined) return;
-    if (!heading) return;
+    if (y === undefined || !heading) return;
     process.nextTick(() => scrollBox.scrollTo(heading.y + scrollBox.scrollTop));
     tocMenu.setSelectedIndex(headingIndexForToc);
     console.log(headingIndexForToc);
@@ -1079,7 +1082,7 @@ const keyHandler = (key: KeyEvent) => {
     return;
   }
   //#endregion
-  //#region table of contents
+  //#region on press T - table of contents
   if (key.name === "t" && (key.capsLock ? !key.shift : key.shift)) {
     tocBox.visible = !tocBox.visible;
     process.nextTick(() => {
@@ -1093,21 +1096,61 @@ const keyHandler = (key: KeyEvent) => {
     return;
   }
   //#endregion
-  //#region focus toc
+  //#region on press t - focus toc
   if (key.name === "t" && (key.capsLock ? key.shift : !key.shift)) {
     if (tocBox.visible) {
       tocMenu.focused
         ? root.findDescendantById("root-scrollbox")!.focus()
         : tocMenu.focus();
     }
-    console.log(tocMenu.focused);
+    console.log("focused tocMenu?:", tocMenu.focused);
+    return;
+  }
+  //#endregion
+  //#region on press tab - focus next element
+  // NOTE: chatgpt generated this too
+  if (key.name === "tab") {
+    const focusable = [
+      root.findDescendantById("root-scrollbox")!,
+      tocMenu,
+    ].filter((child) => child.visible);
+    if (focusable.length === 0) return;
+    const current = focusable.findIndex((child) => child.focused);
+    const direction = key.shift ? -1 : 1;
+    const next = (current + direction + focusable.length) % focusable.length;
+    focusable[next]?.focus();
     return;
   }
   //#endregion
 };
+//#endregion
+
+//#region handle menu
+tocMenu.on(
+  SelectRenderableEvents.SELECTION_CHANGED,
+  (_: number, option: SelectOption) => {
+    const scrollBox = root.findDescendantById(
+      "root-scrollbox",
+    ) as ScrollBoxRenderable;
+    const id = option.value;
+    if (!id) return;
+    const heading = root.findDescendantById(id);
+    const y = heading?.y;
+    if (y === undefined || !heading) return;
+    process.nextTick(() => scrollBox.scrollTo(heading.y + scrollBox.scrollTop));
+    console.log(headingIndexForToc);
+    headingIndexForToc = (headingIndexForToc + 1) % headingsArrayForToc.length;
+  },
+);
+//#endregion
+
+//#region handle tab/shift-tab
+root.on("keypress", (key) => {
+  console.log("tab");
+});
+//#endregion
 
 renderer.keyInput.on("keypress", keyHandler);
 renderer.once("destroy", () => {
   renderer.keyInput.off("keypress", keyHandler);
 });
-//#endregion
